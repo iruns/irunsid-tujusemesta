9f3350f069aa94c1855b9c9ed28aedadceaebcac
iruns <irun.siregar@gmail.com>
2025-03-24 09:25:02 +0700

    /api/v4_update

--- api/v4/base_241026.php
+++ api/v4/base_241026.php
@@ -225,8 +225,8 @@ switch ($path) {
 
     $query_wheres = array();
     $query_wheres_all = array(
-      "name IS NOT NULL",
-      "persona=0"
+      "name IS NOT NULL"//,
+      // "persona=0"
     );
 
     if (isset($_POST['event']))
