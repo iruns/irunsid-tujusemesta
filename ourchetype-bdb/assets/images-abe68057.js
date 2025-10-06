ba1c975802459afbd2f94c58c690e515e4d886ed
iruns <irun.siregar@gmail.com>
2025-10-06 23:58:42 +0700

    /ourchetype-bdb_update

--- ourchetype-bdb/assets/images-abe68057.js
+++
@@ -1,1 +0,0 @@
-import{Q as o}from"./index-6afd404f.js";const c={},t=[[o.Ca,o.Cr,o.En],[o.Ev,o.Ex,o.He],[o.In,o.Lo,o.Ma],[o.Re,o.Ru,o.Sa]];t.forEach((a,r)=>{a.forEach((e,s)=>{c[e]={x:s,y:r}})});export{c as i};
