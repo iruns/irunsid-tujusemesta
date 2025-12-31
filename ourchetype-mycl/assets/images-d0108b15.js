e0925d57358e68449c062e777f87bf3bc43387c0
iruns <irun.siregar@gmail.com>
2026-01-01 03:04:22 +0700

    /ourchetype-mycl_update

---
+++ ourchetype-mycl/assets/images-d0108b15.js
@@ -0,0 +1,1 @@
+import{S as o}from"./index-83ca33eb.js";const c={},t=[[o.Ca,o.Cr,o.En],[o.Ev,o.Ex,o.He],[o.In,o.Lo,o.Ma],[o.Re,o.Ru,o.Sa]];t.forEach((a,r)=>{a.forEach((e,s)=>{c[e]={x:s,y:r}})});export{c as i};
